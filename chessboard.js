cbba5ab456f4ee3d36125808e56910d9704a0547
Dom Dopico <domdopico@Doms-MacBook-Pro.local>
2019-03-30 20:27:17 -0700

    fix typo

--- chessboard.js
+++ chessboard.js
@@ -3,7 +3,6 @@ let board = '';
 
 for (let j = 0; j < size; j++) {
     if (j % 2 == 0) {
-        //board = '';
         for (let i = 0; i < size; i++) {
             if (i % 2 == 0) {
                 board += '#';
