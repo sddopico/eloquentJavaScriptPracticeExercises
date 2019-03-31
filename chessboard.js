3ec8d0c3d7c58fbd7c31af66e4080fb736d4211f
Dom Dopico <domdopico@Doms-MacBook-Pro.local>
2019-03-30 20:11:00 -0700

    first hack with log

--- chessboard.js
+++ chessboard.js
@@ -1,4 +1,4 @@
-let board;
+let board = ' ';
 
 for (let i = 0; i < 8; i++) {
     if (i % 2 == 0) {
@@ -8,4 +8,6 @@ for (let i = 0; i < 8; i++) {
     }
 }
 
-board += '/n';
+board += '\n';
+
+console.log(board);
